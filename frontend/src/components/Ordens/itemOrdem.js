58edb8d73dc6a49b69cdbb050b75884693538882
pmbaldaia <pmbaldaia@outlook.com>
2023-05-16 18:48:24 +0100

    updateitemOrdem

--- frontend/src/components/Ordens/itemOrdem.js
+++ frontend/src/components/Ordens/itemOrdem.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import { Table } from "react-bootstrap";
 import swal from "sweetalert";
 import { Pencil, Trash, CaretLeft } from "@phosphor-icons/react";
 import { useNavigate } from "react-router-dom";
 import "./itemOrdem.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function OrdemItem({ ordem }) {
   const backgroundColor =
@@ -51,7 +51,7 @@ function OrdemItem({ ordem }) {
             <Pencil size={25} weight="light" />
           </Link>
           &nbsp;
-          <Link /* onClick={startDeleteHandler} */>
+          <Link onClick={startDeleteHandler}>
             <Trash size={25} weight="light" />
           </Link>
         </h2>
@@ -64,29 +64,147 @@ function OrdemItem({ ordem }) {
         </h5>
       </div>
       <p>{}</p>
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>QUANTIDADE</th>
-            <th>LIBERADO</th>
-            <th>DATA ENTREGA</th>
-            <th>PRODUTO</th>
-            <th>PRIORIDADE</th>
-            <th>ESTADO</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{ordem.quantidade}</td>
-            <td>{ordem.liberado}</td>
-            <td>{ordem.data_entrega}</td>
-            <td>{ordem.produto}</td>
-            <td>{ordem.prioridade}</td>
-            <td>{ordem.estado}</td>
-          </tr>
-        </tbody>
-      </Table>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>TIPO</Col>
+              <Col>{ordem.tipo}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>SAP</Col>
+              <Col>{ordem.sap}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Txt Breve Material</Col>
+              <Col>{ordem.txt_breve_material}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>Ordem de venda</Col>
+              <Col>{ordem.ordem_venda}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Estação</Col>
+              <Col>{ordem.estacao}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Nome Cliente</Col>
+              <Col>{ordem.nome_cliente}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>Quantidade</Col>
+              <Col>{ordem.quantidade}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Quantidade Fornecida</Col>
+              <Col>{ordem.quantidade_fornecida}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Quantidade Expedida </Col>
+              <Col>{ordem.quantidade_experidida}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>Nome Confeção </Col>
+              <Col>{ordem.nome_confecao}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Liberado </Col>
+              <Col>{ordem.liberado}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Nome Tecido </Col>
+              <Col>{ordem.nome_tecido}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>1º Consumo </Col>
+              <Col>{ordem.primeiro_consumo}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Último Consumo </Col>
+              <Col>{ordem.ultimo_consumo}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Data Prevista Entrada Tecido </Col>
+              <Col>{ordem.data_prevista_entrada_tecido}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>Última Entrada Pedido Compra </Col>
+              <Col>{ordem.ultima_entrada_pedido_compra}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Data Prevista Acessórios </Col>
+              <Col>{ordem.data_prevista_acessorios}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Data Aprovação Modelo </Col>
+              <Col>{ordem.data_aprovacao_modelo}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Row>
+              <Col>Data Desejada Remessa </Col>
+              <Col>{ordem.data_desejada_remessa}</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <Row>
+              <Col>Data Prevista Produção </Col>
+              <Col>{ordem.data_prevista_producao}</Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} style={{ textAlign: "justify" }}>
+            Observações {ordem.observacao}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
